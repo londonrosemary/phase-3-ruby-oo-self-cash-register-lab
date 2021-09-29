560d0a1dfaec13ae5862fc6ec2d11c3b67e29dfd
London <london.rosemary@gmail.com>
2021-09-28 20:30:49 -0400

    commit tree a2c5b6aacf18f86956559c9dfa1c4458dabe93d8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+    attr_accessor :total, :discount, :items, :last_transaction
 
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(name, price, quantity=1)
+        self.last_transaction = price * quantity
+        self.total += self.last_transaction
+        self.items << name
+    end
+end
